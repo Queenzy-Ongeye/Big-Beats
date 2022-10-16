2030618eedb04deec22eb57e640e353c7022b611
Queen-02 <queenzyblack176@gmail.com>
2022-10-16 20:59:29 +0300

    Added fonts and a splash screen

--- lib/splash.dart
+++ lib/splash.dart
@@ -1,18 +1,46 @@
+import 'dart:js';
+
+import 'package:bigbeats/main.dart';
 import 'package:flutter/material.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
 
   @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    navigateToHome();
+  }
+
+  void navigateToHome()async {
+    await Future.delayed(Duration(milliseconds: 1500), () {});
+    Navigator.pushReplacement(this.context, MaterialPageRoute(builder: (context)=> MyApp()));
+  }
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text("Big Beats",
-            style: TextStyle(
-              background:
+        body: Container(
+          height: double.infinity,
+          width: double.infinity,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("assets/splash.jpg"), fit: BoxFit.fill)),
+          child: Center(
+            child: Text(
+              "Big Beats",
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: "Jua",
+                fontSize: 65,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-        ),
-      )
-    );
+          ),
+    ));
   }
 }
