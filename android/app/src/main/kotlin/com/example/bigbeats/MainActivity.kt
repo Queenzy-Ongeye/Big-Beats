daf96542de4b85b454da0d0f5e1ef79ee67141f4
Queen-02 <queenzyblack176@gmail.com>
2022-10-16 18:47:07 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/bigbeats/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bigbeats
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splash.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text("Big Beats",
+            style: TextStyle(
+              background:
+            ),
+        ),
+      )
+    );
+  }
+}
